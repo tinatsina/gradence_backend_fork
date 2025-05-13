5c76e06b8049934ad057519408f4a5909e226f8c
athenadanwa <kuitsdan@gmail.com>
2025-05-13 11:18:40 +0200

    added a new comment

--- src/main/java/com/gradence/ga/controller/ExamController.java
+++ src/main/java/com/gradence/ga/controller/ExamController.java
@@ -35,7 +35,7 @@ public class ExamController {
         this.examRepository = examRepository;
     }
 
-    // Create a new exam
+    // Create a new exam here
     @PostMapping
     public Exam createExam(@Valid @RequestBody Exam exam) {
               exam.setCreatedAt(LocalDateTime.now());
